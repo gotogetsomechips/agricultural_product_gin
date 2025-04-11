540ac21240b8129ff52257edfba3bf92b5803759
yuzhouchen <universe0602@outlook.com>
2025-04-11 20:39:27 +0800

    完善product业务

--- main.go
+++ main.go
@@ -36,7 +36,7 @@ func main() {
 
 	// 配置CORS中间件
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173", "http://localhost:3030", "http://75249acd.r3.cpolar.top"},
+		AllowOrigins:     []string{"http://localhost:5173", "http://localhost:3030"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
